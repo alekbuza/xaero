8cd3ee90b68fcefcdcfc2ae78801dfb48d3d2d28
Aleksandar Buza <67834411+alekbuza@users.noreply.github.com>
2025-05-01 17:37:00 +0200

    chore(copier): update copier base template
    Signed-off-by: Aleksandar Buza <67834411+alekbuza@users.noreply.github.com>

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -7,27 +7,34 @@ SHELL_NAME=$(basename "$SHELL")
 
 export PATH="$HOME/.local/share/mise/shims:$PATH"
 export MISE_ENV="dev"
+export MISE_VERBOSE=1
+export MISE_COLOR=false
 
 echo "[ * ] running mise installer..."
 curl -fsSL https://mise.run | sh
 
 case "$SHELL_NAME" in
-  bash) SHELL_RC="$HOME/.bashrc" ;;
-  zsh) SHELL_RC="$HOME/.zshrc" ;;
+  bash)
+    SHELL_RC="$HOME/.bash_profile"
+    echo "eval \"\$($(realpath ~)/.local/bin/mise activate bash)\"" >> "$(realpath ~)/.bashrc"
+    echo "eval \"\$($(realpath ~)/.local/bin/mise activate --shims)\"" >> "$(realpath ~)/.bash_profile"
+    . "$SHELL_RC"
+    ;;
+  zsh)
+    SHELL_RC="$HOME/.zprofile"
+    echo "eval \"\$($(realpath ~)/.local/bin/mise activate zsh)\"" >> "$(realpath ~)/.zshrc"
+    echo "eval \"\$($(realpath ~)/.local/bin/mise activate --shims)\"" >> "$(realpath ~)/.zprofile"
+    . "$SHELL_RC"
+    ;;
   *)
     echo "[ - ] Unsupported shell: $SHELL_NAME. Supported shells are bash and zsh"
     exit 1
     ;;
 esac
 
-if [ -n "$SHELL_RC" ]; then
-  echo "eval \"\$(mise activate $SHELL_NAME)\"" >> "$SHELL_RC"
-  . "$SHELL_RC"
-fi
-
 mise trust
-mise activate --shims
 mise doctor
-mise install
+mise -E mise install --quiet
+mise install --quiet
 mise run install
 mise run hygiene
